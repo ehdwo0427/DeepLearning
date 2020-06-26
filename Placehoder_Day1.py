aec1f440e27512b09c2069000baa45eca219dcea
ehdwo0427 <ehdwo0427@naver.com>
2020-06-26 23:03:54 +0900

    2020/06/26

---
+++ Placehoder_Day1.py
@@ -0,0 +1,8 @@
+import tensorflow.compat.v1 as tf
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b  # tf.add(a + b)
+
+tf.print(adder_node, feed_dict={a: 3, b: 4.5})
+tf.print(adder_node, feed_dict={a: [1,3], b: [2,4]})
