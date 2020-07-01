258854c5e53f5e6977ac59c3e4c057ac56c08464
ehdwo0427 <ehdwo0427@naver.com>
2020-07-01 16:16:38 +0900

    2020/07/01

---
+++ practice-03.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+tf.set_random_seed(777)
+
+xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
+
+print(x_data.shape, x_data, len(x_data))
+print(y_data.shape, y_data)
+
+X = tf.placeholder(tf.float32, shape=[None, 3])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.random_normal([3, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = tf.matmul(X, W) + b #use matrix
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5).minimize(cost) #add train + optimizer
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for step in range(2001):
+        cost_val, hy_val, _ = sess.run([cost, hypothesis, optimizer], feed_dict={X: x_data, Y: y_data})
+        if step % 20 == 0:
+            print(step, "Cost: ", cost_val,"\nPrediction:\n", hy_val)
+    print("Your score will be ", sess.run(hypothesis, feed_dict={X: [[100, 70, 101]]})) #[[181.73~]]
+    print("Other scores will be ", sess.run(hypothesis, feed_dict={X: [[60, 70, 110], [90, 100, 80]]})) #[[145.86~] [187.23~]]
